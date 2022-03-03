e8dd12ee102789dceeb79cecdf03cd484bf4786d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-03 14:18:09 -0600

    schema created migrations done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,2 @@
 class Movie < ApplicationRecord
-<<<<<<< HEAD
-  has_many :bookmarks
-  validates :title, :overview, presence: true
-  validates :title, uniqueness: true
-=======
->>>>>>> ab2f1d8dc22abce77ddcffa2a8324fdb9ac87702
 end
