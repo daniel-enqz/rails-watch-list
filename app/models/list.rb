f960c95477b12f56c66fe079e82fadf92d4451ee
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-03 14:35:44 -0600

    passed

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
