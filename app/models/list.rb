89520d68e393b549bec2f145f744448b399511bd
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-04 15:14:04 -0600

    cards finished

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, length: { maximum: 15 }
   validates :name, presence: true
   validates :name, uniqueness: true
 end
