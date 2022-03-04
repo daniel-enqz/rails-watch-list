b39269c4df6ab8fee2437da5fc1b548814f3c5d5
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-04 17:36:00 -0600

    category cards fixed, still missing image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,10 @@
-require "json"
-require "rest-client"
+require 'net/http'
+require 'json'
 
-10.times do |i|
-  response = RestClient.get "http://tmdb.lewagon.com/movie/#{i + 1}.json"
+20.times do |i|
+  url = "http://tmdb.lewagon.com/movie/#{i + 1}"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
   repos = JSON.parse(response)
-  # => repos is an `Array` of `Hashes`.
-  Movie.create(title: repos["title"], overview: repos["overview"], rating: repos["rating"])
+  Movie.create(title: repos["title"], overview: repos["overview"], rating: repos["popularity"])
 end
-
-# Movie.create(title: 'Wonder Woman 1984',
-#              overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
-#              poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
-#              rating: 6.9
-#             )
