65c67f457416da7a7f95d6c86c3f058ced367dde
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-05 15:35:49 -0600

    delete button

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
+
+  resources :lists, only: :destroy
 end
