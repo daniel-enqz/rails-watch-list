7428194e7e5a49f6d1fab6d9cafe91642a914f96
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-05 18:04:12 -0600

    route updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
 
-  resources :lists, only: :destroy
+  resources :bookmarks, only: :destroy
 end
