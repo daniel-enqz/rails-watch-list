9476467fcc4dc41ab2007522be43fa8065f48c98
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-05 21:15:11 -0600

    cloudinary configured before test

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_many_attached :photo
   validates :name, length: { maximum: 15 }
   validates :name, presence: true
   validates :name, uniqueness: true
