428a44e2ed8da20ccf88395db3ec5003b5c5bd68
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-09 17:01:21 -0600

    fix delete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,7 +8,6 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
     @bookmark.list = @list
-
     if @bookmark.save
       redirect_to @list
     else
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,8 +1,10 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+
   has_one_attached :photo
   validates :name, length: { maximum: 15 }
+
   validates :name, presence: true
   validates :name, uniqueness: true
 end
